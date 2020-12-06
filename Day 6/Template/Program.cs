e05a2086a0fdbd512d9f6a8df74ae516a8416db4
nick.xue15@gmail.com <nick.xue15@gmail.com>
2020-12-06 15:30:34 +0000

    Add day 6

---
+++ Day 6/Template/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("./input.txt");
+
+            var groupStrings = text.Split("\r\n\r\n");
+
+            var counts = groupStrings
+                .Select(g => g.Split("\r\n")
+                    .Select(s => s.Trim())
+                    .Aggregate((s1, s2) =>  new string(s1.Intersect(s2).ToArray()))
+                    .Distinct()
+                    .Count()
+                );
+
+            Console.WriteLine(counts.Sum());
+        }
+    }
+}
