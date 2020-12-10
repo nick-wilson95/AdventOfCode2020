4974b6f533061139895f1f65f328c78b128f59cd
Nicholas Wilson <nicholas.wilson@tempcover.com>
2020-12-10 17:14:21 +0000

    add day 10

---
+++ Day 10/Template/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("./input.txt");
+
+            var values = text.Split("\r\n")
+                .Select(x => int.Parse(x))
+                .ToList();
+
+            values.Sort();
+
+
+            // Part 1:
+            var jumps = new List<int>();
+
+            var previousValue = 0;
+            for (var i = 0; i < values.Count(); i++)
+            {
+                jumps.Add(values[i] - previousValue);
+                previousValue = values[i];
+            }
+
+            jumps.Add(3);
+
+            var answer1 = jumps.Count(j => j == 1) * jumps.Count(j => j == 3);
+            Console.WriteLine(answer1);
+
+
+            // Part 2:
+            var arrangementsToSocket = new double[values.Last()];
+
+            for (var i = 0; i < values.Count(); i++)
+            {
+                var adapterValue = values[i];
+                var arrangements = 0d;
+                if (adapterValue <= 3) arrangements++;
+
+                for (var j = 1; j <= 3; j++)
+                {
+                    if (adapterValue - 1 - j < 0) break;
+                    arrangements += arrangementsToSocket[adapterValue - 1 - j];
+                }
+
+                arrangementsToSocket[adapterValue - 1] = arrangements;
+            }
+
+            var answer2 = arrangementsToSocket[values.Last() - 1];
+            Console.WriteLine(answer2);
+        }
+    }
+}
---
+++ Day 9/Template/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("./input.txt");
+
+            var values = text.Split("\r\n")
+                .Select(l => double.Parse(l))
+                .ToArray();
+
+            // Part 1:
+            double answer1 = 0;
+            for (var i = 0; i < values.Length - 26; i++)
+            {
+                var preamble = values.Skip(i).Take(25);
+                var pairwiseSums = preamble.SelectMany(x => preamble, (x, y) => x + y);
+
+                var target = values[i + 25];
+
+                if (!pairwiseSums.Contains(target))
+                {
+                    answer1 = target;
+                    break;
+                }
+            }
+
+            Console.WriteLine(answer1);
+
+            // Part 2:
+            var startIndex = 0;
+            var endIndex = 1;
+            while(true)
+            {
+                var sum = values.Skip(startIndex).Take(endIndex + 1 - startIndex).Sum();
+
+                if (sum < answer1) endIndex++;
+                else if (sum > answer1) startIndex++;
+                else break;
+            }
+
+            var range = values.Skip(startIndex).Take(endIndex + 1 - startIndex);
+            var answer2 = range.Min() + range.Max();
+
+            Console.WriteLine(answer2);
+        }
+    }
+}
