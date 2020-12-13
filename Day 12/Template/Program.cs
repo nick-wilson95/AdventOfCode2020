eb22e0646d30c3790cbb95beb84b1a64efe44a3a
nick.xue15@gmail.com <nick.xue15@gmail.com>
2020-12-13 21:08:08 +0000

    add day 13

---
+++ Day 12/Template/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("./input.txt");
+
+            var commands = text.Split("\r\n")
+                .Select(x => new Command(x))
+                .ToArray();
+
+            var answer1 = CalculateAnswer1(commands);
+            WriteAnswer(1, answer1.ToString());
+
+            var answer2 = CalculateAnswer2(commands);
+            WriteAnswer(2, answer2.ToString());
+        }
+
+        private static int CalculateAnswer1(Command[] commands)
+        {
+            var north = 0;
+            var east = 0;
+            var angle = 0;
+
+            foreach (var command in commands)
+            {
+                switch (command.Action)
+                {
+                    case 'N':
+                        north += command.Value; break;
+                    case 'S':
+                        north -= command.Value; break;
+                    case 'E':
+                        east += command.Value; break;
+                    case 'W':
+                        east -= command.Value; break;
+                    case 'L':
+                        angle += command.Value; break;
+                    case 'R':
+                        angle -= command.Value; break;
+                    case 'F':
+                        switch (Mod(angle, 360))
+                        {
+                            case 0:
+                                east += command.Value; break;
+                            case 90:
+                                north += command.Value; break;
+                            case 180:
+                                east -= command.Value; break;
+                            case 270:
+                                north -= command.Value; break;
+                        }
+                        break;
+                }
+            }
+
+            return Math.Abs(east) + Math.Abs(north);
+        }
+
+        private static int CalculateAnswer2(Command[] commands)
+        {
+            var wpOffsetEast = 10;
+            var wpOffsetNorth = 1;
+
+            var north = 0;
+            var east = 0;
+
+            foreach (var command in commands)
+            {
+                switch (command.Action)
+                {
+                    case 'N':
+                        wpOffsetNorth += command.Value; break;
+                    case 'S':
+                        wpOffsetNorth -= command.Value; break;
+                    case 'E':
+                        wpOffsetEast += command.Value; break;
+                    case 'W':
+                        wpOffsetEast -= command.Value; break;
+                    case 'F':                        
+                        north += wpOffsetNorth * command.Value;
+                        east += wpOffsetEast * command.Value;
+                        break;
+                    default:
+                        var angle = command.Value * (command.Action == 'L' ? 1 : -1);
+                        switch (Mod(angle, 360))
+                        {
+                            case 90:
+                                Switch(ref wpOffsetEast, ref wpOffsetNorth);
+                                wpOffsetEast = -wpOffsetEast;
+                                break;
+                            case 180:
+                                wpOffsetEast = -wpOffsetEast;
+                                wpOffsetNorth = -wpOffsetNorth;
+                                break;
+                            case 270:
+                                Switch(ref wpOffsetEast, ref wpOffsetNorth);
+                                wpOffsetNorth = -wpOffsetNorth;
+                                break;
+                        }
+                        break;
+                }
+            }
+
+            return Math.Abs(east) + Math.Abs(north);
+        }
+
+        private class Command
+        {
+            public char Action { get; }
+            public int Value { get; }
+
+            public Command(string commandString)
+            {
+                Action = commandString[0];
+                Value = int.Parse(commandString.Skip(1).ToArray());
+            }
+        }
+        private static int Mod(int x, int m)
+        {
+            return (x % m + m) % m;
+        }
+
+        private static void Switch(ref int a, ref int b)
+        {
+            var x = a;
+            a = b;
+            b = x;
+        }
+
+        private static void WriteAnswer(int part, string answer)
+        {
+            Console.WriteLine($"Answer for part {part}:");
+            Console.WriteLine(answer);
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Day 13/Template/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("./input.txt");
+
+            var lines = text.Split("\r\n");
+
+            // Part 1:
+            var startTime = int.Parse(lines[0]);
+
+            var busIds = lines[1].Split(',')
+                .Where(x => x != "x")
+                .Select(int.Parse);
+
+            var delays = busIds.Select(x => x - startTime % x);
+            var answer1 = busIds.Zip(delays)
+                .Where(x => x.Second == delays.Min())
+                .Select(x => x.First * x.Second)
+                .First();
+
+            WriteAnswer(1, answer1.ToString());
+
+            // Part 2:
+            var busList = lines[1].Split(',')
+                .Select(Bus.Create)
+                .Where(x => x.Active)
+                .OrderBy(x => x.Interval)
+                .Reverse()
+                .ToList();
+
+            var pivot = busList[0].Position;
+            busList.ForEach(x => x.Position -= pivot);
+
+            var baseInterval = busList[0].Interval;
+            var bussesToCheck = busList.Skip(1)
+                .ToArray();
+
+            var counter = 0d;
+            var departure = 0d;
+
+            while (true)
+            {
+                counter++;
+
+                departure = counter * baseInterval;
+
+                var validTime = true;
+                for (var i = 0; i < bussesToCheck.Length; i++)
+                {
+                    var bus = bussesToCheck[i];
+                    if ((departure + bus.Position) % bus.Interval != 0)
+                    {
+                        validTime = false;
+                        break;
+                    };
+                }
+
+                if (validTime) break;
+            }
+
+            WriteAnswer(2, departure.ToString());
+        }
+
+        private class Bus
+        {
+            public double Interval { get; private set; }
+            public double Position { get; set; }
+            public bool Active { get; private set; }
+
+            public static Bus Create(string id, int position)
+            {
+                var isActive = int.TryParse(id, out var interval);
+
+                return new Bus { Interval = interval, Position = position, Active = isActive };
+            }
+        }
+
+        private static void WriteAnswer(int part, string answer)
+        {
+            Console.WriteLine($"Answer for part {part}:");
+            Console.WriteLine(answer);
+            Console.WriteLine("");
+        }
+    }
+}
